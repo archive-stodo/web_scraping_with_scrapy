62b050a30ff6ddb6c7a773cfa55d4a10ee43c310
dawid <stodoo@gmail.com>
2019-12-24 02:06:24 +0100

    following links to next pages and scraping

--- finviz/spiders/finviz_spider.py
+++ finviz/spiders/finviz_spider.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # to run, just run
 # PycharmProjects/finviz/finviz$ sscrapy crawl spider-name -o output.csv
+from scrapy import Selector
 
 
 class FinvizSpider(scrapy.Spider):
@@ -18,17 +19,23 @@ class FinvizSpider(scrapy.Spider):
 
         dark_rows = content.xpath('.//tr[@class="table-dark-row-cp"]')
         light_rows = content.xpath('.//tr[@class="table-light-row-cp"]')
-
         parsed_dark_rows = self.parseRows(dark_rows)
         parsed_light_rows = self.parseRows(light_rows)
-
         parsed_rows = parsed_light_rows + parsed_dark_rows
 
         df = pd.DataFrame(parsed_rows)
-        print('hrhrhrhr')
+        df.set_index('id', inplace=True)
+        print('I am here')
         print(df)
-
         yield {'df': df}
+
+        next_page_a_href = content.css('a[href].tab-link').extract()[-1]
+        sel = Selector(text = next_page_a_href)
+        link = 'https://finviz.com/' + sel.css('a::attr(href)').extract()[0]
+        print('next_page_link ', link)
+
+        yield scrapy.Request(url = link, callback = self.parse)
+
 
     def parseRows(self, rows):
         rows_text = rows.css('td.screener-body-table-nw ::text').extract()

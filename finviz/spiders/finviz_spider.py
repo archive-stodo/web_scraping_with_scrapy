27921b951c2bcd2ad2a346ee3fab3e9d9752ae53
dawid <stodoo@gmail.com>
2019-12-28 16:24:59 +0100

    gitignore

--- finviz/spiders/finviz_spider.py
+++ finviz/spiders/finviz_spider.py
@@ -10,17 +10,22 @@ from scrapy import Selector
 class FinvizSpider(scrapy.Spider):
     name = 'spider-name'
 
+    # https://finviz.com/screener.ashx?v=171&f=cap_smallover,fa_div_o3&o=industry
+
     screener_url = 'https://finviz.com/screener.ashx'
     overview_screen = '?v=111'
     valuation_screen = '?v=121'
     financial_screen = '?v=161'
     ownership_screen = '?v=131'
+    technical_screen = '?v=171'
+
     not_small_with_good_dividend = '&f=cap_smallover,fa_div_o3&o=industry'
 
-    overview_url = screener_url + overview_screen + not_small_with_good_dividend
-    valuation_url = screener_url + valuation_screen + not_small_with_good_dividend
-    financial_url = screener_url + financial_screen + not_small_with_good_dividend
-    ownership_url = screener_url + ownership_screen + not_small_with_good_dividend
+    overview_url = screener_url + overview_screen
+    valuation_url = screener_url + valuation_screen
+    financial_url = screener_url + financial_screen
+    ownership_url = screener_url + ownership_screen
+    technical_url = screener_url + technical_screen
 
     def __init__(self):
         super().__init__()
@@ -28,11 +33,41 @@ class FinvizSpider(scrapy.Spider):
         logging.info('This will get logged')
 
     def start_requests(self):
+        url_filter = FinvizSpider.not_small_with_good_dividend
+
+        yield scrapy.Request(url=FinvizSpider.technical_url, callback=self.scrap_technical)
         yield scrapy.Request(url=FinvizSpider.overview_url, callback=self.scrap_overview)
-        # uncomment to scrap other indicators
-        # yield scrapy.Request(url=FinvizSpider.valuation_url, callback=self.scrap_valuation)
-        # yield scrapy.Request(url=FinvizSpider.financial_url, callback=self.scrap_financial)
-        # yield scrapy.Request(url=FinvizSpider.ownership_url, callback=self.scrap_ownership)
+        yield scrapy.Request(url=FinvizSpider.valuation_url, callback=self.scrap_valuation)
+        yield scrapy.Request(url=FinvizSpider.financial_url, callback=self.scrap_financial)
+        yield scrapy.Request(url=FinvizSpider.ownership_url, callback=self.scrap_ownership)
+
+    def scrap_technical(self, response):
+        content, parsed_rows = self.parse_data_rows_on_website(response, self.parse_technical_columns)
+        self.save_rows_array_to_file(parsed_rows, 'technical.txt')
+
+        link = self.get_next_page_link(content)
+
+        yield scrapy.Request(url=link, callback=self.scrap_technical)
+
+    def parse_technical_columns(self, response):
+        rows_text = response.css('td.screener-body-table-nw ::text').extract()
+        rows_text = self.divide_chunks(rows_text, 15)
+
+        parsed_rows = []
+        for index, row in enumerate(rows_text):
+            row_dict = dict()
+            row_dict['id'] = row[0]
+            row_dict['ticker'] = row[1]
+
+            row_dict['beta'] = row[2]
+            row_dict['sma20'] = row[4]
+            row_dict['sma50'] = row[5]
+            row_dict['sma200'] = row[6]
+            row_dict['rsi'] = row[9]
+
+            parsed_rows.append(row_dict)
+
+        return parsed_rows
 
     def scrap_ownership(self, response):
         content, parsed_rows = self.parse_data_rows_on_website(response, self.parse_ownership_columns)
